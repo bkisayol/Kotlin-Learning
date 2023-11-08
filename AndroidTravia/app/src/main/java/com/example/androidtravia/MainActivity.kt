df6b9ab29693e1e0134b5308bdda4bd0993507aa
Berkay Kısayol <b.kisayol@gmail.com>
2023-11-08 23:20:44 +0300

    Android trivia first version

---
+++ AndroidTravia/app/src/main/java/com/example/androidtravia/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.example.androidtravia
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
+import com.example.androidtravia.databinding.ActivityMainBinding
+
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        @Suppress("UNUSED_VARIABLE")
+        val binding = DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
+    }
+
+}
